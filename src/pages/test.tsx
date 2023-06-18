4d6811cf3631a9f76f604571aaf5c3d1cbcee1f3
Markus Löffler <markus.loeffler01@gmail.com>
2023-06-18 11:48:59 +0200

    First test successful

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import OwnButton from "../components/Button";
+
+
+export default function Test() {
+    return (
+        <div>
+            <h1>Test</h1>
+            <OwnButton />
+        </div>
+    )
+}
